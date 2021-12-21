b386f7c365224761cb1043a25a86685d1a1b14c3
YunSungJungMillion <jys77408288@gmail.com>
2021-12-21 22:46:11 +0900

    테스트

--- testChilProcess/testChilProcess.cpp
+++ testChilProcess/testChilProcess.cpp
@@ -2,48 +2,70 @@
 //
 
 #include <iostream>
-#include <Windows.h>
 #include <vector>
 #include <string>
 #include <iostream>
-#define BUFF 1024
+#include <algorithm>
 using namespace std;
-
-struct time {
-	int start;
-	int end;
+struct Score {
+	int A;
+	int B;
+	int dif;
 };
 
-int main()
+bool customSorting(Score const& rhs, Score const& lhs)
 {
-	
-	int input;
-	
-	cin >> input;
-	cout << input << endl;
-	
-	vector<time> vT;
-	for (int i = 0; i < input; i++)
+	if (rhs.dif != lhs.dif)
 	{
-		int str , str2;
-		cin >> str;
-		cout << str << endl;
-		cin >> str2;
-		cout << str2 << endl;
+		return rhs.dif > lhs.dif;
 	}
-	int wait;
-	
-	Sleep(4000);
+	else {
+		return  rhs.A > lhs.A;
+	}
 
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+int main()
+{
+
+	int testCase = 0;
+	cin >> testCase;
+
+	for (int i = 0; i < testCase; i++)
+	{
+		int inputCnt = 0;
+		cin >> inputCnt;
+		vector<Score> vScore;
+
+		for (int j = 0; j < inputCnt; j++)
+		{
+			Score score;
+			cin >> score.A >> score.B;
+			if (score.A > score.B)
+			{
+				score.dif = score.A - score.B;
+			}
+			else
+			{
+				score.dif = score.B - score.A;
+			}
+			vScore.push_back(score);
+
+		}
+
+		sort(vScore.begin(), vScore.end(), customSorting);
+
+		for (auto &ele : vScore)
+		{
+			cout << "A : " << ele.A << " B: " << ele.B << " DIF : " << ele.dif << endl;
+		}
+
+
+	}
+
+	return 1;
+
+}
+
+
